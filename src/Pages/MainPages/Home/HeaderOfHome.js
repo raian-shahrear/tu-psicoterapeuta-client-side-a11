ac6b6bb0f1b3b712effc4e17fc74ad9edf9cf844
raian-shahrear <raianshahrearbd@gmail.com>
2022-11-08 20:38:27 +0600

    patient-promlems added in home page

--- src/Pages/MainPages/Home/HeaderOfHome.js
+++ src/Pages/MainPages/Home/HeaderOfHome.js
@@ -7,13 +7,13 @@ const HeaderOfHome = () => {
     <div>
       <section className="bg-gray-200 text-gray-700">
         <div className="lg:w-11/12 mx-auto px-4 md:px-24 lg:px-0 flex flex-col justify-center items-center sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-          <div className="flex flex-col justify-center  text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+          <div className="flex flex-col justify-center py-16 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
             <h1 className="text-5xl font-bold leading-none sm:text-6xl">
               <p>Hello! I'm your</p>
               <p className="text-green-700 mt-2">Dr. Amir Ahmad</p>
             </h1>
             <div className="mt-6 mb-8 text-lg sm:mb-12">
-              <p>I am a psychologist and my goal is to partner with you to enhance the quality of your life.</p>
+              <p>I am a psychotherapist and my goal is to partner with you to enhance the quality of your life.</p>
               <p>Whether you are dealing with something specific or are seeking increased satisfaction and balance in life, I’m here to help.</p>
             </div>
             <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
--- src/Pages/MainPages/Home/Home.js
+++ src/Pages/MainPages/Home/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 import HeaderOfHome from "./HeaderOfHome";
+import prob1 from "../../../Resources/problems/anxiety.png";
+import prob2 from "../../../Resources/problems/weigh-scale.png";
+import prob3 from "../../../Resources/problems/no-smoking.png";
+import prob4 from "../../../Resources/problems/depression.png";
+
 
 const Home = () => {
   return (
@@ -7,8 +12,30 @@ const Home = () => {
       <header>
         <HeaderOfHome />
       </header>
-      <section className="lg:w-11/12 mx-auto px-4 md:px-24 lg:px-0">
-        hello from home
+      <section className="lg:w-11/12 mx-auto px-4 md:px-24 lg:px-0 mt-20">
+        <h2 className="text-4xl font-semibold text-green-700 mb-6">If you have any of these...</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-24">
+          <div className="bg-green-100 flex flex-col items-center text-center p-6 rounded-sm">
+            <img src={prob1} alt="prob1" className="w-16" />
+            <p className="text-xl font-semibold mt-1">Anxiety</p>
+            <p className="text-sm mt-3">Stop procrastinating and boost self-confidence today</p>
+          </div>
+          <div className="bg-blue-100 flex flex-col items-center text-center p-6 rounded-sm">
+            <img src={prob2} alt="prob2" className="w-16" />
+            <p className="text-xl font-semibold mt-1">Obesity</p>
+            <p className="text-sm mt-3">See how I can help you change your eating habits</p>
+          </div>
+          <div className="bg-yellow-100 flex flex-col items-center text-center p-6 rounded-sm">
+            <img src={prob3} alt="prob3" className="w-16" />
+            <p className="text-xl font-semibold mt-1">Smoking Cessation</p>
+            <p className="text-sm mt-3">Quit smoking permanently with my services.</p>
+          </div>
+          <div className="bg-red-100 flex flex-col items-center text-center p-6 rounded-sm">
+            <img src={prob4} alt="prob4" className="w-16" />
+            <p className="text-xl font-semibold mt-1">Post Trauma</p>
+            <p className="text-sm mt-3">I respect my patients' secret and can help them to recover from PTSD</p>
+          </div>
+        </div>
       </section>
     </div>
   );
